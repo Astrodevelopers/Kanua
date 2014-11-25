514c4917f0b5e3a689b35e9d5c8d21143a6023d6
claubm777 <claubm777@hotmail.com>
2014-11-25 13:37:31 -0500

    buscar noticia por titulo

--- src/KanuaUML12.web/src/main/java/co/edu/uniandes/csw/astroDevelopers/noticia/master/service/NoticiaMasterService.java
+++ src/KanuaUML12.web/src/main/java/co/edu/uniandes/csw/astroDevelopers/noticia/master/service/NoticiaMasterService.java
@@ -37,12 +37,20 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import co.edu.uniandes.csw.astroDevelopers.noticia.master.logic.dto.NoticiaMasterDTO;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 @Path("/NoticiaMaster")
 @Stateless
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class NoticiaMasterService extends _NoticiaMasterService {
-
+    @GET
+    @Path("/buscarNoticiasPorTitulo")
+    public String buscarNoticiasPorTitulo(@QueryParam("titulo") String titulo) {
+        return noticiaLogicService.buscarNoticiasPorTitulo(titulo);
+    }
 
 }
