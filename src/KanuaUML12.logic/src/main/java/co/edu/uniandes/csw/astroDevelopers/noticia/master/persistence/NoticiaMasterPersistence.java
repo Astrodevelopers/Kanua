ff5082f7cd8fbf6221e3703fde354d9eb5c42e42
claubm777 <claubm777@hotmail.com>
2014-11-25 13:39:55 -0500

    buscar noticia por titulo

--- src/KanuaUML12.logic/src/main/java/co/edu/uniandes/csw/astroDevelopers/noticia/master/persistence/NoticiaMasterPersistence.java
+++ src/KanuaUML12.logic/src/main/java/co/edu/uniandes/csw/astroDevelopers/noticia/master/persistence/NoticiaMasterPersistence.java
@@ -35,10 +35,52 @@ import javax.ejb.Stateless;
 import co.edu.uniandes.csw.astroDevelopers.noticia.master.persistence.api.INoticiaMasterPersistence;
 import javax.ejb.LocalBean;
 import javax.enterprise.inject.Default;
+import java.util.List;
+import co.edu.uniandes.csw.astroDevelopers.noticia.persistence.converter.NoticiaConverter;
+import co.edu.uniandes.csw.astroDevelopers.noticia.logic.dto.NoticiaDTO;
+import javax.persistence.Query;
 
 @Default
 @Stateless 
 @LocalBean
 public class NoticiaMasterPersistence extends _NoticiaMasterPersistence  implements INoticiaMasterPersistence {
-
+    public String buscarNoticiasPorTitulo(String titulo) {
+        // https://access.redhat.com/documentation/en-US/JBoss_Enterprise_Application_Platform/5/html/RESTEasy_Reference_Guide/Using__Path_and__GET___POST__etc..html
+        // Query q = entityManager.createQuery("select u from ProyectoEntity u inner join Proyectotag_proyectoEntity s on s.proyectoId=u.id inner join TagEntity t on t.id=s.tag_proyectoId where t.name=:valortag");
+        // q.setParameter("valortag",tag);
+        String titulos[]=titulo.split("\\;");
+        String condicion="";
+        for (int i=0; i<titulos.length; i++)
+        {
+            if (!condicion.isEmpty())
+            {
+                condicion+=" OR ";
+            }
+            condicion+="u.name=:valortitulo"+i;
+        }
+        Query q=null;
+        if (titulo.isEmpty())
+        {
+            q = entityManager.createQuery("select u from NoticiaEntity u");
+        }
+        else
+        {
+            q = entityManager.createQuery("select u from NoticiaEntity u  where "+condicion);
+            for(int j=0; j<titulos.length; j++)
+            {
+                q.setParameter("valortitulo"+j,titulos[j]);
+            }
+        }
+        List<NoticiaDTO> noticias=NoticiaConverter.entity2PersistenceDTOList(q.getResultList());
+        String ids="";
+        for(NoticiaDTO noticia:noticias)
+        {
+            if(!ids.isEmpty())
+            {
+                ids+=",";
+            }
+            ids+=noticia.getId();
+        }
+        return ids;
+    }
 }
