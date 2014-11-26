5668203a27333e487977f3ae3e4dd2c53bc33f8e
deortiz10 <de.ortiz10@uniandes.edu.co>
2014-11-26 11:41:30 -0500

    validaciones

--- src/KanuaUML12.web/src/main/webapp/src/model/noticiaModel.js
+++ src/KanuaUML12.web/src/main/webapp/src/model/noticiaModel.js
@@ -32,12 +32,12 @@ define(['model/_noticiaModel'], function() {
 
  	validate: function(attrs,options){
              var nom=attrs.imagen.toString();
-             var url= nom.search("http://");
+             var url= nom.search("http:");
             var validationMessage = "";
             if(!attrs.name){
                 validationMessage = "El nombre no puede ser vacio. ";
             }  
-            if (url=-1||!attrs.imagen)
+            if (0>nom.indexOf("http:")||!attrs.imagen)
             {
                 validationMessage+="Imagen no valida o vacio. ";
                 
