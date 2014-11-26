41f5c6d93092540b5c70774016382965fad3b190
TheBlackTux <diegorbaquero@gmail.com>
2014-11-26 02:42:16 -0500

    pagina Principal

--- src/KanuaUML12.web/src/main/webapp/pp.js
+++ src/KanuaUML12.web/src/main/webapp/pp.js
@@ -1,6 +1,10 @@
 var app = angular.module('kanua',[]);
 
-app.controller('proyectos', function($scope, $http) {
+app.config(function($locationProvider) {
+  $locationProvider.html5Mode(true);
+})
+
+app.controller('proyectos', function($scope, $http, $log, $location) {
     $scope.proyectos = [];
 
     $scope.getProyectos = function() {
@@ -8,6 +12,18 @@ app.controller('proyectos', function($scope, $http) {
             $scope.proyectos = data;
         });
     };
-
+    
     $scope.getProyectos();
 });
+
+app.controller('mc',  function($scope, $http, $log, $location) {
+    $scope.busquedas = [{name: 'Taller 1', content: 'Blas Blas Blas'},{name: "Noticia 3", content: 'Blas Blas Blas'}];
+    
+    $scope.query = $location.search()['q'];
+    
+    if($location.search()['q']) {
+        $scope.ifr = false;
+    } else {
+        $scope.ifr = true;
+    }
+})
