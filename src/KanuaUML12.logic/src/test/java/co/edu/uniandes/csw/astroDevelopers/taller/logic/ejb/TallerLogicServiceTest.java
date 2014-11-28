b5910b0738fa9fca79588dcd5048c3f64cd72c99
claubm777 <claubm777@hotmail.com>
2014-11-27 19:01:12 -0500

    pruebas taller

--- src/KanuaUML12.logic/src/test/java/co/edu/uniandes/csw/astroDevelopers/taller/logic/ejb/TallerLogicServiceTest.java
+++ src/KanuaUML12.logic/src/test/java/co/edu/uniandes/csw/astroDevelopers/taller/logic/ejb/TallerLogicServiceTest.java
@@ -54,6 +54,28 @@ import co.edu.uniandes.csw.astroDevelopers.taller.persistence.entity.TallerEntit
 import co.edu.uniandes.csw.astroDevelopers.taller.persistence.converter.TallerConverter;
 import static co.edu.uniandes.csw.astroDevelopers.util._TestUtil.*;
 
+import co.edu.uniandes.csw.astroDevelopers.taller.master.logic.api.ITallerMasterLogicService;
+import co.edu.uniandes.csw.astroDevelopers.taller.master.persistence.api.ITallerMasterPersistence;
+import co.edu.uniandes.csw.astroDevelopers.taller.master.persistence.TallerMasterPersistence;
+import co.edu.uniandes.csw.astroDevelopers.taller.master.logic.ejb.TallerMasterLogicService;
+import co.edu.uniandes.csw.astroDevelopers.taller.master.logic.dto.TallerMasterDTO;
+
+import co.edu.uniandes.csw.astroDevelopers.tag.logic.api.ITagLogicService;
+import co.edu.uniandes.csw.astroDevelopers.tag.persistence.TagPersistence;
+import co.edu.uniandes.csw.astroDevelopers.tag.persistence.api.ITagPersistence;
+import co.edu.uniandes.csw.astroDevelopers.tag.persistence.entity.TagEntity;
+import co.edu.uniandes.csw.astroDevelopers.tag.persistence.converter.TagConverter;
+import co.edu.uniandes.csw.astroDevelopers.tag.logic.dto.TagDTO;
+import co.edu.uniandes.csw.astroDevelopers.tag.logic.ejb.TagLogicService;
+import co.edu.uniandes.csw.astroDevelopers.taller.master.persistence.entity.Tallertag_tallerEntity;
+import co.edu.uniandes.csw.astroDevelopers.usuario.logic.api.IUsuarioLogicService;
+import co.edu.uniandes.csw.astroDevelopers.usuario.logic.dto.UsuarioDTO;
+import co.edu.uniandes.csw.astroDevelopers.usuario.logic.ejb.UsuarioLogicService;
+import co.edu.uniandes.csw.astroDevelopers.usuario.persistence.UsuarioPersistence;
+import co.edu.uniandes.csw.astroDevelopers.usuario.persistence.api.IUsuarioPersistence;
+import co.edu.uniandes.csw.astroDevelopers.usuario.persistence.converter.UsuarioConverter;
+import co.edu.uniandes.csw.astroDevelopers.usuario.persistence.entity.UsuarioEntity;
+import co.edu.uniandes.csw.astroDevelopers.taller.master.persistence.entity.TallerusuarioEntity;
 @RunWith(Arquillian.class)
 public class TallerLogicServiceTest {
 
@@ -67,7 +89,30 @@ public class TallerLogicServiceTest {
 				.addPackage(TallerPersistence.class.getPackage())
 				.addPackage(TallerEntity.class.getPackage())
 				.addPackage(ITallerPersistence.class.getPackage())
+                                .addPackage(ITallerMasterLogicService.class.getPackage())
+                                .addPackage(ITallerMasterPersistence.class.getPackage())
+                                .addPackage(TallerMasterPersistence.class.getPackage())
+                                .addPackage(TallerMasterLogicService.class.getPackage())
+                                .addPackage(TagLogicService.class.getPackage())
+				.addPackage(ITagLogicService.class.getPackage())
+				.addPackage(TagPersistence.class.getPackage())
+				.addPackage(TagEntity.class.getPackage())
+				.addPackage(ITagPersistence.class.getPackage())
+                                .addPackage(UsuarioLogicService.class.getPackage())
+				.addPackage(IUsuarioLogicService.class.getPackage())
+				.addPackage(UsuarioPersistence.class.getPackage())
+				.addPackage(UsuarioEntity.class.getPackage())
+				.addPackage(IUsuarioPersistence.class.getPackage())
+                .addPackage(UsuarioDTO.class.getPackage())
+                .addPackage(UsuarioConverter.class.getPackage())
+                .addPackage(UsuarioEntity.class.getPackage())
+                .addPackage(TagDTO.class.getPackage())
+                .addPackage(Tallertag_tallerEntity.class.getPackage())
+                .addPackage(TagConverter.class.getPackage())
+                .addPackage(TagEntity.class.getPackage())
+                .addPackage(TallerusuarioEntity.class.getPackage())
                 .addPackage(TallerDTO.class.getPackage())
+                .addPackage(TallerMasterDTO.class.getPackage())
                 .addPackage(TallerConverter.class.getPackage())
                 .addPackage(TallerEntity.class.getPackage())
 				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
@@ -79,7 +124,18 @@ public class TallerLogicServiceTest {
 	
 	@Inject
 	private ITallerPersistence tallerPersistence;	
-
+        
+        	@Inject
+	private ITagLogicService tagLogicService;
+        
+        @PersistenceContext
+	private EntityManager em;
+        
+        @Inject
+	private ITallerMasterPersistence tallerMasterPersistence;
+        
+        @Inject
+        protected ITallerMasterLogicService tallerMasterLogicService;
 	@Before
 	public void configTest() {
 		try {
@@ -208,6 +264,120 @@ public class TallerLogicServiceTest {
 		Assert.assertEquals(ldto.getPublicacion(), resp.getPublicacion());	
 	}
 	
-	
+	 @Test
+	public void buscarCharlaTest(){
+            
+                TallerDTO ldto=new TallerDTO();
+		ldto.setName(generateRandom(String.class));
+		ldto.setTitulo(generateRandom(String.class));
+		ldto.setInformacion(generateRandom(String.class));
+		ldto.setFechaEvento(generateRandomDate());
+		ldto.setImagen(generateRandom(String.class));
+		ldto.setPublicacion(generateRandomDate());
+		TallerDTO result=tallerLogicService.createTaller(ldto);
+                
+		TagDTO tag1=new TagDTO();
+                tag1.setName("clau");
+                TagDTO resultag=tagLogicService.createTag(tag1);
+                
+		Assert.assertNotNull(result);
+                Assert.assertNotNull(resultag);
+                
+		String resultado1=tallerMasterLogicService.buscarTallersPorTag("blublublublu");
+                Assert.assertEquals("",resultado1);
+                Tallertag_tallerEntity chtag=new Tallertag_tallerEntity(result.getId(),resultag.getId());
+                System.out.println("000***********************************  ");
+                tallerMasterPersistence.createTallertag_tallerEntity(chtag);
+		System.out.println("111***********************************  "+chtag.getTallerId());
+                
+                String resultado=tallerMasterLogicService.buscarTallersPorTag("clau");
+               // Assert.assertEquals("",resultado);
+                System.out.println("222***********************************  "+resultado);
+                Assert.assertEquals(resultado, chtag.getTallerId()+"");
+                
+        
+        
+	}
+        
+	@Test
+        public void buscarCharlaForTest(){
+            for(int i=0; i<20; i++)
+            {
+               TallerDTO ldto=new TallerDTO();
+		ldto.setName(generateRandom(String.class));
+		ldto.setTitulo(generateRandom(String.class));
+		ldto.setInformacion(generateRandom(String.class));
+		ldto.setFechaEvento(generateRandomDate());
+		ldto.setImagen(generateRandom(String.class));
+		ldto.setPublicacion(generateRandomDate());
+		TallerDTO result=tallerLogicService.createTaller(ldto);
+                
+		TagDTO tag1=new TagDTO();
+                tag1.setName("clau"+i);
+                TagDTO resultag=tagLogicService.createTag(tag1);
+                
+		Assert.assertNotNull(result);
+                Assert.assertNotNull(resultag);
+                
+		String resultado1=tallerMasterLogicService.buscarTallersPorTag("holaholaholahola"+i);
+                Assert.assertEquals("",resultado1);
+                Tallertag_tallerEntity chtag=new Tallertag_tallerEntity(result.getId(),resultag.getId());
+                System.out.println("000***********************************  ");
+                tallerMasterPersistence.createTallertag_tallerEntity(chtag);
+		
+                String resultado=tallerMasterLogicService.buscarTallersPorTag("clau"+i);
+                Assert.assertEquals(resultado, chtag.getTallerId()+"");
+            }
+        }
+        
+        @Test
+        public void buscarCharlasTest(){
+            
+                TallerDTO ldto=new TallerDTO();
+		ldto.setName(generateRandom(String.class));
+		ldto.setTitulo(generateRandom(String.class));
+		ldto.setInformacion(generateRandom(String.class));
+		ldto.setFechaEvento(generateRandomDate());
+		ldto.setImagen(generateRandom(String.class));
+		ldto.setPublicacion(generateRandomDate());
+                TallerDTO ldto2=new TallerDTO();
+		ldto2.setName(generateRandom(String.class));
+		ldto2.setTitulo(generateRandom(String.class));
+		ldto2.setInformacion(generateRandom(String.class));
+		ldto2.setFechaEvento(generateRandomDate());
+		ldto2.setImagen(generateRandom(String.class));
+		ldto2.setPublicacion(generateRandomDate());
+		TallerDTO result1=tallerLogicService.createTaller(ldto);
+                TallerDTO result2=tallerLogicService.createTaller(ldto2);
+            
+		TagDTO tag1=new TagDTO();
+                TagDTO tag2=new TagDTO();
+                
+                tag1.setName("clau");
+                tag2.setName("dani");
+                TagDTO resultag1=tagLogicService.createTag(tag1);
+                TagDTO resultag2=tagLogicService.createTag(tag2);
+		Assert.assertNotNull(result1);
+                Assert.assertNotNull(result2);
+                Assert.assertNotNull(resultag1);
+                Assert.assertNotNull(resultag2);
+            
+		String resultado1=tallerMasterLogicService.buscarTallersPorTag("holahola");
+                Assert.assertEquals("",resultado1);
+                Tallertag_tallerEntity chtag1=new Tallertag_tallerEntity(result1.getId(),resultag1.getId());
+                Tallertag_tallerEntity chtag2=new Tallertag_tallerEntity(result2.getId(),resultag2.getId());
+                //System.out.println("000***********************************  ");
+                tallerMasterPersistence.createTallertag_tallerEntity(chtag1);
+                tallerMasterPersistence.createTallertag_tallerEntity(chtag2);
+                
+                String resultado=tallerMasterLogicService.buscarTallersPorTag("clau;dani");
+                //System.out.println("222222222***********************************  "+resultado+"+++++++ "+result1.getId()+""+result2.getId());
+                
+                Assert.assertEquals(result1.getId()+","+result2.getId(),resultado);
+                //System.out.println("222222222***********************************  "+resultado+"+++++++ "+result1.getId()+""+result2.getId());
+                
+            
+            
+        }
 	
 }
