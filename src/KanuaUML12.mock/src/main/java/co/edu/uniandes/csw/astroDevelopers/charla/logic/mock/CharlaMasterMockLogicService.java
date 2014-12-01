93b8e8d2df35ea2468f2f55269f0f51d8157d7a9
scvalencia <sc.valencia606@gmail.com>
2014-11-30 22:48:08 -0500

    Busqueda por charla

--- src/KanuaUML12.mock/src/main/java/co/edu/uniandes/csw/astroDevelopers/charla/logic/mock/CharlaMasterMockLogicService.java
+++ src/KanuaUML12.mock/src/main/java/co/edu/uniandes/csw/astroDevelopers/charla/logic/mock/CharlaMasterMockLogicService.java
@@ -39,4 +39,8 @@ public class CharlaMasterMockLogicService extends _CharlaMasterMockLogicService 
     public String buscarCharlaPorTag(String tag){
         return "";
     }	
+
+    public String buscarCharlas(String tag) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
